f3da3e58fae76caaeec9e0157c5289e413ff020c
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2021-12-02 00:15:23 +0100

    Day 1, part 2

--- 01/src/lib.rs
+++ 01/src/lib.rs
@@ -1,21 +1,42 @@
 use ::eyre::{eyre, Result};
+use ::itertools::Itertools;
 use ::std::io::BufRead;
 
-pub fn solve(input: &mut dyn BufRead) -> Result<()> {
-    let mut lines = input.lines();
-    if let Some(first_line) = lines.next() {
-        let mut count = 0;
-        let mut previous_measurement = first_line?.parse::<usize>()?;
-        for line in lines {
-            let this_measurement = line?.parse::<usize>()?;
-            if this_measurement > previous_measurement {
-                count += 1;
-            }
-            previous_measurement = this_measurement;
-        }
-        println!("{}", count);
-        Ok(())
-    } else {
-        Err(eyre!("no input lines"))
+pub fn solve_a(input: &mut dyn BufRead) -> Result<()> {
+    let count_increasing_pairs = numbers_from_lines(input)?
+        .iter()
+        .tuple_windows::<(_, _)>()
+        .filter(|(prev, next)| prev < next)
+        .count();
+    println!("{}", count_increasing_pairs);
+    Ok(())
+}
+
+pub fn solve_b(input: &mut dyn BufRead) -> Result<()> {
+    let count_increasing_window_avrg = numbers_from_lines(input)?
+        .iter()
+        .tuple_windows::<(_, _, _)>()
+        .map(|w| w.0 + w.1 + w.2)
+        .tuple_windows::<(_, _)>()
+        .filter(|(prev, next)| prev < next)
+        .count();
+    println!("{}", count_increasing_window_avrg);
+    Ok(())
+}
+
+fn numbers_from_lines(input: &mut dyn BufRead) -> Result<Vec<usize>> {
+    let mut numbers = Vec::new();
+    for number_or_err in input.lines().enumerate().map(|(n, maybe_line)| {
+        let line_count = n + 1;
+        maybe_line
+            .map_err(|io_err| eyre!("error reading line {}: {}", line_count, io_err))
+            .and_then(|line| {
+                line.parse::<usize>().map_err(|parse_err| {
+                    eyre!("error parsing line {} as usize: {}", line_count, parse_err)
+                })
+            })
+    }) {
+        numbers.push(number_or_err?);
     }
+    Ok(numbers)
 }
--- aoc2021/src/main.rs
+++ aoc2021/src/main.rs
@@ -16,24 +16,28 @@ struct Options {
     /// Use '-' to read from STDIN.
     #[clap(short, long, default_value = "input")]
     input_folder: String,
+    /// use this option to calculate the second puzzle of the day
+    #[clap(short)]
+    b: bool,
 }
 
 fn main() {
     let options = Options::parse();
     if options.input_folder == "-" {
-        solve(options.day, &mut BufReader::new(stdin()));
+        solve(options.day, options.b, &mut BufReader::new(stdin()));
     } else {
         let filepath = Path::new(&options.input_folder).join(format!("{:02}", options.day));
         match File::open(&filepath) {
-            Ok(file) => solve(options.day, &mut BufReader::new(file)),
+            Ok(file) => solve(options.day, options.b, &mut BufReader::new(file)),
             Err(err) => eprintln!("Cannot open {:?}: {}", filepath, err),
         }
     };
 }
 
-fn solve(day: usize, input: &mut dyn BufRead) {
-    match day {
-        1 => day_01::solve(input),
+fn solve(day: usize, part2: bool, input: &mut dyn BufRead) {
+    match (day, part2) {
+        (1, false) => day_01::solve_a(input),
+        (1, true) => day_01::solve_b(input),
         _ => {
             eprintln!("Day {} is not implemented.", day);
             Ok(())
